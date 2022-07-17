fd5f095e102c50fb11b10f3283baeb07d5b63d8c
ounguder <ounguder@gmail.com>
2022-07-17 18:57:41 +0200

    search option was added by using useState

--- src/index.js
+++ src/index.js
@@ -3,16 +3,30 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { books } from "./booklist";
 import Book from "./Book";
+import Search from "./search";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Shelf() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const searchHandler = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedBooks = books.filter(function (book) {
+    return book.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
-    <section className="bookList">
-      {books.map((book) => {
-        return <Book key={book.id} {...book} />;
-      })}
-    </section>
+    <div>
+      <h1 className="myHeader">My Book List</h1>
+      <Search onSearch={searchHandler} />
+      <section className="bookList">
+        {searchedBooks.map((book) => {
+          return <Book key={book.id} {...book} />;
+        })}
+      </section>
+    </div>
   );
 }
 root.render(
